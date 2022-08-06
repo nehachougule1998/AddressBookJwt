233f7a80f231e25c8faf6f886e01287766e88456
nehachougule1998 <nchougule734@gmail.com>
2022-08-06 22:03:11 +0530

    Added

---
+++ AddressBook_JWT/src/main/java/com/bridgelabz/springsecurityjwt/AddressBookJwtApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookJwtApplication.class, args);
+    }
+
+}
